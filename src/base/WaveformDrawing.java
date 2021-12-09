c43a9b887f95e1524f0920a1489204ccb4bab91f
El-Mundo <superbluecat71@yahoo.com>
2021-12-09 18:18:01 +0800

    First upload.

---
+++ src/base/WaveformDrawing.java
@@ -0,0 +1,21 @@
+package base;
+
+import processing.core.PApplet;
+
+public class WaveformDrawing extends PApplet {	
+
+	public static void main(String args[]) {
+		PApplet.main("base.WaveformDrawing");
+	}
+	
+	@Override
+	public void settings() {
+		size(720, 480, P2D);
+	}
+	
+	@Override
+	public void setup() {
+		
+	}
+
+}
